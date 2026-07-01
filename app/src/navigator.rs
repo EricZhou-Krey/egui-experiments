82696bf454439ff3328124a412638d7a9e4fc08b
Eric Zhou <ericzhou1101@gmail.com>
2026-07-01 18:11:32 +0100

    ideation about navigation

--- app/src/navigator.rs
+++ app/src/navigator.rs
@@ -9,6 +9,12 @@ impl Viewable for Navigator {
     }
     fn draw_ui(&mut self, ui: &mut egui::Ui) {
         ui.heading("Hello from Nav");
+        // WORKING BRANCH
+        // Current working idea:
+        // Isometric landscape to right side where there are tech sci-y nodes that are hoverable to
+        // to give a preview or static logo or etc for the project, connected with concepts and
+        // stuff on the right
+        // Settings to configure the angle, colour and etc of the scene
     }
 
     fn is_closeable(&self) -> bool {
