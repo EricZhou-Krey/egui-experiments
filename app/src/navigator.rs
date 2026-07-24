bf59cd6a920567debab4e577672a961a4c806437
Eric Zhou <ericzhou1101@gmail.com>
2026-07-24 19:36:30 +0100

    visual feedback

--- app/src/navigator.rs
+++ app/src/navigator.rs
@@ -1,29 +1,56 @@
-use glam::{I8Vec2, Vec2};
+use egui::{Pos2, Vec2};
+use glam::I8Vec2;
 use shared_view::Viewable;
 
-#[derive(Default)]
 pub struct TriangulationGraph {
-    init_flag: bool, // Memory alignment issues :(
+    width: f32,
+    height: f32,
+
     n_points: usize,
-    points: Vec<Vec2>,
-    point_speeds: Vec<Vec2>,
+    points: Vec<Pos2>,
+
+    point_size: f32,
+    point_color: egui::Color32,
+    point_velocity: Vec<Vec2>,
+
+    edge_length: f32,
+    edge_color: egui::Color32,
     edges: Vec<I8Vec2>,
 }
 
+impl Default for TriangulationGraph {
+    fn default() -> Self {
+        Self {
+            width: 0.0,
+            height: 0.0,
+
+            n_points: 100,
+            points: Vec::with_capacity(100),
+
+            point_size: 4.0,
+            point_color: egui::Color32::RED,
+            point_velocity: (0..100)
+                .map(|_| Vec2 {
+                    x: (rand::random::<f32>() - 0.5) * 0.5,
+                    y: (rand::random::<f32>() - 0.5) * 0.5,
+                })
+                .collect(),
+
+            edge_length: 2.0,
+            edge_color: egui::Color32::LIGHT_RED,
+            edges: Vec::with_capacity(100),
+        }
+    }
+}
+
 impl TriangulationGraph {
-    pub fn re_initialize(&mut self, n_points: usize, point_speed: f32, dimensions: Vec2) {
-        self.n_points = n_points;
+    pub fn re_initialize(&mut self, dimensions: Vec2) {
+        self.width = dimensions.x;
+        self.height = dimensions.y;
         self.points = (0..self.n_points)
-            .map(|_| Vec2 {
+            .map(|_| Pos2 {
                 x: rand::random::<f32>() * dimensions.x,
                 y: rand::random::<f32>() * dimensions.y,
-            })
-            .collect();
-
-        self.point_speeds = (0..self.n_points)
-            .map(|_| Vec2 {
-                x: (rand::random::<f32>() - 0.5) * point_speed,
-                y: (rand::random::<f32>() - 0.5) * point_speed,
             })
             .collect();
 
@@ -32,11 +59,32 @@ impl TriangulationGraph {
 
     fn update_points(&mut self) {
         for i in 0..self.points.len() {
-            self.points[i] += self.point_speeds[i];
+            self.points[i] += self.point_velocity[i];
+            self.points[i].x = self.points[i].x.rem_euclid(self.width);
+            self.points[i].y = self.points[i].y.rem_euclid(self.height);
         }
     }
 
-    fn update_edges(&mut self) {}
+    fn delaunay_triangulation(points: &[Pos2], left: usize, right: usize) -> Vec<I8Vec2> {
+        let mut sorted_indicies: Vec<usize> = (0..points.len()).collect();
+        sorted_indicies.sort_unstable_by(|&i, &j| {
+            (points[i].x, points[i].y)
+                .partial_cmp(&(points[j].x, points[j].y))
+                .unwrap()
+        });
+
+        (1..points.len())
+            .map(|i| I8Vec2 {
+                x: sorted_indicies[i - 1] as i8,
+                y: sorted_indicies[i] as i8,
+            })
+            .collect()
+    }
+
+    fn update_edges(&mut self) {
+        self.edges =
+            TriangulationGraph::delaunay_triangulation(&self.points, 0, self.points.len() - 1);
+    }
 }
 
 impl Viewable for TriangulationGraph {
@@ -45,23 +93,35 @@ impl Viewable for TriangulationGraph {
     }
 
     fn draw_ui(&mut self, ui: &mut egui::Ui) {
-        if !self.init_flag {
-            self.re_initialize(
-                100,
-                0.1,
-                Vec2 {
-                    x: ui.available_width(),
-                    y: ui.available_height(),
-                },
-            );
-            self.init_flag = true;
+        let full_size: Vec2 = ui.available_size();
+
+        if full_size.x != self.width || full_size.y != self.height {
+            self.re_initialize(full_size);
         }
 
         self.update_points();
         self.update_edges();
 
-        let debug_info: String = format!("points: {:?}, \n edges: {:?}", self.points, self.edges);
-        ui.heading(debug_info.as_str());
+        let (response, painter) =
+            ui.allocate_painter(ui.available_size(), egui::Sense::click_and_drag());
+
+        painter.rect_filled(response.rect, 0.0, egui::Color32::from_gray(30));
+
+        let edge_style: egui::Stroke = egui::Stroke {
+            width: self.edge_length,
+            color: self.edge_color,
+        };
+
+        for edge in &self.edges {
+            painter.line_segment(
+                [self.points[edge.x as usize], self.points[edge.y as usize]],
+                edge_style,
+            );
+        }
+
+        for pos in self.points.clone() {
+            painter.circle_filled(pos, self.point_size, self.point_color);
+        }
     }
 }
 
