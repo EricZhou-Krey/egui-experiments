db34d34f4f92fdc8b9c47bd50b4c918184d03630
Eric Zhou <ericzhou1101@gmail.com>
2026-07-29 14:48:21 +0100

    plan prompt

--- app/src/navigator.rs
+++ app/src/navigator.rs
@@ -5,6 +5,13 @@ use shared_view::Viewable;
 pub struct Navigator {
     triangulation_graph: TriangulationGraph,
 }
+// PLAN:
+// Selectable point on triangulation triangulation_graph
+//      - Project mouse position to node that has been clicked
+//      - Set velocity to 0
+//      - Present preview at top left or right of the Project
+//      - Title changes to description of the Project
+//          - First example will be the tabletop sound Project
 
 impl Viewable for Navigator {
     fn draw_ui(&mut self, ui: &mut egui::Ui) {
