44c4d44b5659f92c9bd87683c9081940cd5d053f
Eric Zhou <ericzhou1101@gmail.com>
2026-08-24 15:52:39 +0100

    restructed plan, moving to sound interactions

--- experiments/tabletop_sound/src/lib.rs
+++ experiments/tabletop_sound/src/lib.rs
@@ -12,33 +12,29 @@ pub mod tabs;
 TODO: PLAN BETTER
 Consider each of the states of each tab and create them approriately,
 need to create:
-    Tab::MapView,
+     -> Tab::MapView,
     Tab::NodeDetails,
     Tab::Console,
     Tab::NodeTree,
     Tab::SoundView,
     Tab::PlayControls,
 
-- Add macros to make rects, sqaures, and approximate circles, triangles, etc
-- Attach ambient noise to each node irrelevant or applied to fading out linearly/
-or other beizer function defined by user
+* - Add macros to make rects, sqaures, and approximate circles, triangles, etc
+* - Make it look a bit nice
 
-- Make it look a bit nice
+1 - Define receiver and be able move the reveiver around to catpure ambient sound via linear distance or beizer
 
-- Define receiver and be able move the reveiver around to catpure ambient sound
-- Define ray tracer for sound (edit above if necessary)
+2 - Define ray tracer for sound (edit above if necessary)
     - Define emitter and parameters
     - Define ray and parameters
     - Figure out if you can construct sounds using base 3 wavelengths much like colours,
         are there metamers for sounds?
+3 - Workout directional sounds,
+
 - WGPU ray calculations
 
-- Workout directional sounds,
 
-Random notes:
-- For walls specifically, implment quadtree for optimized ray collision search
-- Add responsivness for adding convex hull, add effect for selected nodes
-- Decouple mapview tools usage
-- Redo drawing nodes to the mapview
-- Naive Removal used, should use quadtrees later
+* - For walls specifically, implment quadtree for optimized ray collision search
+* - Decouple mapview tools usage
+* - Naive find used, should use quadtrees later
 */
