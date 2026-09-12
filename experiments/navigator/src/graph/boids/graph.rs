a9d666c631863d6c2519f5dde4604b24b8b05c60
Eric Zhou <ericzhou1101@gmail.com>
2026-09-12 18:51:55 +0100

    planning and reading for optimized boids with wgpu compute shaders

--- experiments/navigator/src/graph/boids/graph.rs
+++ experiments/navigator/src/graph/boids/graph.rs
@@ -8,6 +8,17 @@ pub struct BoidGraph {
 impl BoidGraph {
     pub fn ui(&mut self, ui: &mut egui::Ui) -> Option<GraphUpdate> {
         ui.heading("boid_graph");
+        //TODO currently
+        // Basic implementation, order or operations ->
+        // 1. camera -> projecting into 3d
+        // 2. scene -> starting with a spacital box without obstacles
+        //      -> Holds Quadtree<Boid>
+        //      -> Objects
+        //      -> Bounds (Rect (f32, f32, f32, f32))
+        // 3. octree optimized binary partition of points
+        //      -> just n depth bins that is queried for adjacent, move down in tree structure
+        // 4. boids representation
+        //      -> Boid(Vec2) -> can be directly placed into the octree struct
         None
     }
 
--- experiments/navigator/src/graph/boids/mod.rs
+++ experiments/navigator/src/graph/boids/mod.rs
@@ -1,1 +1,4 @@
+pub mod camera;
 pub mod graph;
+pub mod octree;
+pub mod scene;
